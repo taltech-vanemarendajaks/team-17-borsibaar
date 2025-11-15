c74d7bd73a463caacc6ad7ec01474462093e28b0
TFT <artoreinik@gmail.com>
2025-11-15 16:02:07 +0200

    fix error in setting the form to base state

--- frontend/app/(protected)/inventory/page.tsx
+++ frontend/app/(protected)/inventory/page.tsx
@@ -181,6 +181,8 @@ export default function Inventory() {
         description: "",
         categoryId: "",
         currentPrice: "",
+        minPrice: "",
+        maxPrice: "",
         initialQuantity: "",
         notes: "",
       });
